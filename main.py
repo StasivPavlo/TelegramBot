9fd65e37b85f915bf00cba7359101c2c73126b0a
SnUmnui <vs.anps@gmail.com>
2022-02-12 17:50:05 +0200

    refactor

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
-    await message.reply("ffffffffff")
+    await message.reply("fffffffffffff")
 
 
 @dp.message_handler(commands=['start'])
